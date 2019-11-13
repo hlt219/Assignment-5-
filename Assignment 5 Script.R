cc9bdb0036c7eac2faffe0d8669e4aa792db7e5c
Helana <hlt219@lehigh.edu>
2019-11-13 15:18:59 -0500

    Edited DF script

--- Assignment 5 Script.R
+++ Assignment 5 Script.R
@@ -1,1 +1,9 @@
-DF <- data.table::fread("Assignment 5 data.csv")
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+header <- read.table("Assignment 5 data.csv", header = TRUE,
+                      sep=",",nrow=1)
+
+DF <- fread("Assignment 5 data.csv", skip=1, sep=",",
+                        header=FALSE, data.table=FALSE)
+setnames(DF,colnames(header))
+rm(header)
